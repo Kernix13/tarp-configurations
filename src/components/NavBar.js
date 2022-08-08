a1af2bd9b790b0abb8589eaeb1cbf700d27f1fb4
“Jim” <jimkernicky@gmail.com>
2022-08-08 19:37:29 -0400

    Fucking local merge

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import React, { useState } from "react";
-=======
 import { useState } from "react";
->>>>>>> hamburger-menu
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { FaBars, FaRegWindowClose } from "react-icons/fa";
@@ -10,39 +6,6 @@ import { FaBars, FaRegWindowClose } from "react-icons/fa";
 import MenuItems from "./MenuItems";
 
 function NavBar({ title }) {
-<<<<<<< HEAD
-  const [navbarOpen, setNavbarOpen] = React.useState(false);
-
-  return (
-    <header>
-      <nav className="navbar mb-12 shadow-lg bg-gradient-to-br from-teal-500 to-teal-900 text-neutral-content">
-        <div className="container mx-auto">
-          {/* Need hover class */}
-          <div className="flex-none px-2">
-            <Link to="/" className="text-lg font-bold align-middle">
-              {title}
-            </Link>
-          </div>
-
-          <div className="flex-1 sm:px-2 sm:mx-2">
-            <div className="flex justify-end">
-              <Link to="/" className="btn btn-ghost btn-sm rounded-btn">
-                Home
-              </Link>
-              <Link to="/results" className="btn btn-ghost btn-sm rounded-btn">
-                Results
-              </Link>
-              <Link to="/configs" className="btn btn-ghost btn-sm rounded-btn">
-                Configs
-              </Link>
-              <Link to="/calcs" className="btn btn-ghost btn-sm rounded-btn">
-                Calcs
-              </Link>
-            </div>
-          </div>
-        </div>
-      </nav>
-=======
   const [active, setActive] = useState(false);
   const showMenu = () => {
     setActive(!active);
@@ -87,7 +50,6 @@ function NavBar({ title }) {
           <MenuItems showMenu={showMenu} active={active} />
         </nav>
       </div>
->>>>>>> hamburger-menu
     </header>
   );
 }
